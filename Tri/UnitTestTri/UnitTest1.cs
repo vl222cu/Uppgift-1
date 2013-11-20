3d03d4ed021abea9e360e9c85909c1798ec25c4a
vl222cu <vl222cu@student.lnu.se>
2013-11-20 22:30:44 +0100

    Skapat UnitTest

---
+++ Tri/UnitTestTri/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTri
+{
+	[TestClass]
+	public class TriangleUnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
