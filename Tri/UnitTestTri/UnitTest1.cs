05c3e049e7cb7833560c8515f4894c287b641eb0
vl222cu <vl222cu@student.lnu.se>
2013-11-21 01:59:02 +0100

    Lagt till fler testfall

--- Tri/UnitTestTri/UnitTest1.cs
+++ Tri/UnitTestTri/UnitTest1.cs
@@ -12,6 +12,12 @@ namespace UnitTestTri
 			Triangle tri1 = new Triangle(1.0, 0.5, 1.0);
 			Assert.IsTrue(tri1.isIsosceles());
 		}
+		[TestMethod]
+		public void isIsoscelesTestWhenFalse()
+		{
+			Triangle triFalse = new Triangle(1.0, 1.0, 1.0);
+			Assert.IsFalse(triFalse.isIsosceles());
+		}
 
 		[TestMethod]
 		public void isEquilateralTest()
@@ -21,10 +27,33 @@ namespace UnitTestTri
 		}
 
 		[TestMethod]
+		public void isEquilateralTestWhenFalse()
+		{
+			Triangle triFalse2 = new Triangle(7.0, 10.0, 30.0);
+			Assert.IsFalse(triFalse2.isEquilateral());
+		}
+
+		[TestMethod]
 		public void isScaleneTest()
 		{
-			Triangle tri3 = new Triangle(10.0, 20.0, 30.0);
+			Triangle tri3 = new Triangle(5.0, 30.0, 30.0);
 			Assert.IsTrue(tri3.isScalene());
-		}	
+		}
+
+		[TestMethod]
+		public void isScaleneTestWhenFalse()
+		{
+			Triangle triFalse3 = new Triangle(10.0, 10.0, 10.0);
+			Assert.IsFalse(triFalse3.isScalene());
+		}
+
+		[TestMethod]
+		public void CheckStruct()
+		{
+			int a;
+			int b;
+			Assert.IsTrue(true);
+		}
+		
 	}
 }
